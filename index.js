7ab261cc091dc7368b1a1e8d46e0db097716ddf4
szaszfaijulia <szaszfai.julia@gmail.com>
2020-11-17 14:17:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+//Írj egy error first callback-et meghívó aszinkron fügvényt, ami visszaadja a texts könyvtárban fevő fájlokból kiolvasott szövegekből konkatenált sztringet (a texts könyvtárban bármyennyi szövegfájl lehet)
+//A könyvtár fájl listájának és a szövegfájlok tartalmának kiolvasásához az fs modul aszinkron függvényeit (readdir, readFile) használd
+
+
